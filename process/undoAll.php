a87104fbfe848605ad111cc8b6717d68569d4b7b
Eunseop <eunseop@Eunseopui-iMac.local>
2018-06-22 17:33:56 +0900

    공백을 NO로 수정

--- process/undoAll.php
+++ process/undoAll.php
@@ -3,7 +3,7 @@
 if (defined('__IM__') == false) exit;
 
 // 성공실패여부 판별하기 애매하다. 쿼리가 잘못되면 내부에서 프로그램이 종료돼버린다.
-$success = $this->db()->update($this->table->todolist, array('complete'=>''))->execute();
+$success = $this->db()->update($this->table->todolist, array('complete'=>'NO'))->execute();
 
 if ($success == true) {
     $results->success = true;
